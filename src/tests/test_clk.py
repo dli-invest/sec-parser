efb9a7f627d8b2f6792fbd3e35e7a3e13e3d0a6b
David Li <davidli012345@gmail.com>
2022-11-13 19:53:21 -0800

    fix: update

---
+++ src/tests/test_clk.py
@@ -0,0 +1,19 @@
+import sys
+import unittest
+
+from sec_parser.clk import get_list_of_ciks
+
+
+
+class TestClk(unittest.TestCase):
+
+    def test_clk():
+        df = get_list_of_ciks()
+        assert len(df) > 3
+
+    def test_zim_in_clk():
+        df = get_list_of_ciks()
+        assert df[df['ticker'] == 'ZIM'].iloc[0]['cik'] == '0001654126'
+
+if __name__ == '__main__':
+    unittest.main()
